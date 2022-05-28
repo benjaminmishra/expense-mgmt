c36f742d624c2df86739d2d0f0077deabd60496d
Benjamin Moloise Mishra <benjaminmishra@gmail.com>
2022-05-28 20:20:49 +0530

    Inital commit

---
+++ ExpenseMgmt.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ExpenseMgmt.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+

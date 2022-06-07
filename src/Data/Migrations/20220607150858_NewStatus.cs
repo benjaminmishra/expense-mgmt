21b8f03c4fd02f157bc2a244fdc5fb5bc94f3abb
Benjamin Mishra <benjamin.mishra@ringtailgames.com>
2022-06-07 20:41:35 +0530

    Fixed a few more things

---
+++ src/Data/Migrations/20220607150858_NewStatus.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpenseMgmt.Data.Migrations
+{
+    public partial class NewStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "ExpenseStatusTypes",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 5, "Rejected by manager", "Rejected" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ExpenseStatusTypes",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}

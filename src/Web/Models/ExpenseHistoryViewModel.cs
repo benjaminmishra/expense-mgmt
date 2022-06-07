f55826b0e9bc660e4866117efe0a9ff0030fd2ea
Benjamin Mishra <benjamin.mishra@ringtailgames.com>
2022-06-08 00:00:33 +0530

    Added accountant functionality

---
+++ src/Web/Models/ExpenseHistoryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Web.Models;
+
+public class ExpenseHistoryViewModel
+{
+    public int ExpenseId { get; set; }
+    public string Purpose { get; set; }
+    public string? Remark { get; set; }
+    public int StatusId { get; set; }
+    public string Status { get; set; }
+    public string CreatedBy { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public DateTime ModifiedOn { get; set; }
+}
